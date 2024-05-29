76395b1a598ac8b0ef615e906ea939fcb84a2b42
Christopher Chan <cjchan.chris@gmail.com>
2024-05-29 17:17:44 -0600

    Implement MPL3115A2S Altimeter Driver

---
+++ MPL3115A2S/MPL3115A2S_Driver.c
@@ -0,0 +1,247 @@
+/**
+ ******************************************************************************
+ * File Name          : MPL3115A2S_Driver.c
+ * Description        : MPL3115A2S Driver
+ * Authors            : Chris (cjchanx)
+ ******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "MPL3115A2S_Driver.h"
+
+/* Macros and Constants ------------------------------------------------------*/
+#define MPL3115A2S_ADDR (0xC0) // 8-bit write address for MPL3115A2S
+
+// Registers
+#define MPL3115A2S_STATUS (0x00) // Status Register
+
+#define MPL3115A2S_OUT_P_MSB (0x01) // Pressure Data Out MSB
+#define MPL3115A2S_OUT_P_CSB (0x02) // Pressure Data Out CSB
+#define MPL3115A2S_OUT_P_LSB (0x03) // Pressure Data Out LSB
+
+#define MPL3115A2S_OUT_T_MSB (0x04) // Temperature Data Out MSB
+#define MPL3115A2S_OUT_T_LSB (0x05) // Temperature Data Out LSB
+#define MPL3115A2S_DR_STATUS (0x06) // Data Ready Status
+
+// FIFO Registers
+#define MPL3115A2S_F_SETUP (0x0F) // FIFO Setup
+    #define MPL3115A2S_F_MODE_CIRCULAR (0b01 << 6) // Circular FIFO Mode
+    #define MPL3115A2S_F_MODE_FIFO (0b10 << 6) // FIFO Mode Stop on Full
+
+// Control Registers
+#define MPL3115A2S_CTRL_REG1 (0x26) // Control Register 1
+    #define MPL3115A2S_CTRL_ALT_SHIFT (7) // Altimeter Mode Shift
+    #define MPL3115A2S_CTRL_OSR_SHIFT (3) // Oversampling Ratio Shift
+    #define MPL3115A2S_CTRL_RST (0b1 << 2) // Software Reset
+    #define MPL3115A2S_CTRL_OST (0b1 << 1) // One-Shot
+    #define MPL3115A2S_CTRL_ACTIVE (0b1) // Active Mode Enable (from Standby)
+
+#define MPL3115A2S_PT_DATA_CFG (0x13) // PT Data Configuration
+    #define MPL3115A2S_PT_DATA_CFG_DREM_SHIFT (2) // Data Ready Event Mode Shift
+    #define MPL3115A2S_PT_DATA_CFG_PDEFE_SHIFT (1) // Pressure Data Event Flag Enable Shift
+    #define MPL3115A2S_PT_DATA_CFG_TDEFE_SHIFT (0) // Temperature Data Event Flag Enable Shift
+    #define MPL3115A2S_PT_DATA_CFG_EN_ALL (0b111) // Enable All Flags
+
+#define MPL3115A2S_CTRL_REG3 (0x28) // Control Register 3
+    #define MPL3115A2S_CTRL_REG3_IPOL1_SHIFT (5) // Interrupt 1 Polarity Shift
+    #define MPL3115A2S_CTRL_REG3_PP_OD1_SHIFT (4) // Interrupt 1 Open Drain Shift
+    #define MPL3115A2S_CTRL_REG3_IPOL2_SHIFT (1) // Interrupt 2 Polarity Shift
+    #define MPL3115A2S_CTRL_REG3_PP_OD2_SHIFT (0) // Interrupt 2 Open Drain Shift
+
+#define MPL3115A2S_CTRL_REG4 (0x29) // Control Register 4
+    #define MPL3115A2S_CTRL_REG4_INT_EN_DRDY_SHIFT (7) // Data Ready Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_FIFO_SHIFT (6) // FIFO Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_PW_SHIFT (5) // Pressure Window Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_TW_SHIFT (4) // Temperature Window Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_PTH_SHIFT (3) // Pressure Threshold Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_TTH_SHIFT (2) // Temperature Threshold Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_PCHG_SHIFT (1) // Pressure Change Interrupt Enable Shift
+    #define MPL3115A2S_CTRL_REG4_INT_EN_TCHG_SHIFT (0) // Temperature Change Interrupt Enable Shift
+
+#define MPL3115A2S_WHO_AM_I (0x0C) // Device ID Register
+    #define MPL3115A2S_WHO_AM_I_ID (0xC4) // Device ID
+
+/* Helpers -------------------------------------------------------------------*/
+static uint8_t DecodeConfiguration(MPL3115A2S_Config* config) {
+    return (config->altimeter_mode << MPL3115A2S_CTRL_ALT_SHIFT) | (config->os_ratio << MPL3115A2S_CTRL_OSR_SHIFT);
+}
+static uint8_t DecodeDataConfiguration(MPL3115A2S_Data_Config* config) {
+    if(config->enable_all_flags) 
+        return MPL3115A2S_PT_DATA_CFG_EN_ALL;
+    return (config->data_ready_event_mode << MPL3115A2S_PT_DATA_CFG_DREM_SHIFT) | 
+           (config->pressure_data_event_flag << MPL3115A2S_PT_DATA_CFG_PDEFE_SHIFT) | 
+           (config->temperature_data_event_flag << MPL3115A2S_PT_DATA_CFG_TDEFE_SHIFT);
+}
+static uint8_t DecodeIntConfiguration_CTRL3(MPL3115A2S_Int_Config* config) {
+    return (config->int1_active_high << MPL3115A2S_CTRL_REG3_IPOL1_SHIFT) | 
+           (config->int1_open_drain << MPL3115A2S_CTRL_REG3_PP_OD1_SHIFT) | 
+           (config->int2_active_high << MPL3115A2S_CTRL_REG3_IPOL2_SHIFT) | 
+           (config->int2_open_drain << MPL3115A2S_CTRL_REG3_PP_OD2_SHIFT);
+}
+static uint8_t DecodeIntConfiguration_CTRL4(MPL3115A2S_Int_Config* config) {
+    return (config->data_ready_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_DRDY_SHIFT) | 
+           (config->fifo_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_FIFO_SHIFT) | 
+           (config->pressure_window_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_PW_SHIFT) | 
+           (config->temperature_window_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_TW_SHIFT) | 
+           (config->pressure_threshold_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_PTH_SHIFT) | 
+           (config->temperature_threshold_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_TTH_SHIFT) | 
+           (config->pressure_change_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_PCHG_SHIFT) | 
+           (config->temperature_change_it_enable << MPL3115A2S_CTRL_REG4_INT_EN_TCHG_SHIFT);
+}
+
+static uint32_t DecodePressureAsInt(uint8_t* buf, uint16_t multiplier) {
+    // MSB, CSB, LSB in Q18.2 format
+    uint32_t pressure = (buf[0] << 16) | (buf[1] << 8) | buf[2];
+    pressure >>= 4; // Discard unused bits
+    pressure *= multiplier;
+    pressure /= 4;
+    return pressure;
+}
+static float DecodePressureAsFloat(uint8_t* buf) {
+    // MSB, CSB, LSB in Q18.2 format
+    uint32_t pressure = (buf[0] << 16) | (buf[1] << 8) | buf[2];
+    pressure >>= 4; // Discard unused bits
+    return (float)pressure / 4.0f;
+}
+
+static int32_t DecodeAltitudeAsInt(uint8_t* buf, uint16_t multiplier) {
+    // MSB, CSB, LSB in Q16.4 format
+    int32_t altitude = ((short)((buf[0] << 8) | buf[1]));
+    altitude *= multiplier;
+    altitude += ((buf[2] >> 4) * multiplier) / 16;
+    return altitude;
+}
+static float DecodeAltitudeAsFloat(uint8_t* buf) {
+    // MSB, CSB, LSB in Q16.4 format
+    float altitude = ((short)((buf[0] << 8) | buf[1])) + ((float)(buf[2] >> 4)/16.0f);
+    return altitude;
+}
+
+static int32_t DecodeTemperatureAsInt(uint8_t* buf, uint16_t multiplier) {
+    // MSB, LSB in Q8.4 format
+    int32_t temperature = (short)((buf[0] << 8) | buf[1]);
+    temperature >>= 4; // Discard unused bits
+    temperature *= multiplier;
+    temperature /= 16;
+    return temperature;
+}
+static float DecodeTemperatureAsFloat(uint8_t* buf) {
+    // MSB, LSB in Q8.4 format
+    int16_t temperature = (short)((buf[0] << 8) | buf[1]);
+    temperature >>= 4; // Discard unused bits
+    return (float)temperature / 16.0f;
+}
+
+/* Internal Enums -------------------------------------------------------------*/
+typedef enum {
+    MPL_STATE_NOT_INIT = 0,
+    MPL_STATE_READY_ALT,
+    MPL_STATE_READY_PRES,
+} MPL_STATE;
+
+/* Static Data ----------------------------------------------------------------*/
+static MPL_STATE state = MPL_STATE_NOT_INIT;
+
+/* Initialization Functions -----------------------------------------------------------------*/
+
+MPL_OP_STATUS MPL3115A2S_Init(MPL3115A2S_Config* const cfg, MPL3115A2S_Data_Config* const data_cfg, MPL3115A2S_Int_Config* const int_cfg) {
+    // Check the device ID
+    if (MPL3115A2S_CheckDeviceID() != MPL_OK) return MPL_ERR;
+
+    // Decode the configuration
+    uint8_t ctrl_reg1 = DecodeConfiguration(cfg);
+    uint8_t pt_data_cfg = DecodeDataConfiguration(data_cfg);
+    uint8_t ctrl_reg3 = DecodeIntConfiguration_CTRL3(int_cfg);
+    uint8_t ctrl_reg4 = DecodeIntConfiguration_CTRL4(int_cfg);
+
+    // Write the configuration
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_CTRL_REG1, ctrl_reg1)) return MPL_ERR;
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_PT_DATA_CFG, pt_data_cfg)) return MPL_ERR;
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_CTRL_REG3, ctrl_reg3)) return MPL_ERR;
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_CTRL_REG4, ctrl_reg4)) return MPL_ERR;
+
+    // Set active mode
+    ctrl_reg1 |= MPL3115A2S_CTRL_ACTIVE;
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_CTRL_REG1, ctrl_reg1)) return MPL_ERR;
+
+    if(cfg->altimeter_mode) state = MPL_STATE_READY_ALT;
+    else state = MPL_STATE_READY_PRES;
+
+    return MPL_OK;
+}
+
+/* Read Data Functions (float) ---------------------------------------------------------------*/
+
+MPL_OP_STATUS MPL3115A2S_ReadPressure(float* pressure) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+    else if(state == MPL_STATE_READY_ALT) return MPL_INVALID_SETTING; // Cannot read pressure in altimeter mode
+
+    uint8_t buf[3];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_P_MSB, buf, 3)) return MPL_ERR;
+    *pressure = DecodePressureAsFloat(buf);
+    return MPL_OK;
+}
+
+MPL_OP_STATUS MPL3115A2S_ReadAltitude(float* altitude) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+    else if(state == MPL_STATE_READY_PRES) return MPL_INVALID_SETTING; // Cannot read altitude in barometer mode
+
+    uint8_t buf[3];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_P_MSB, buf, 3)) return MPL_ERR;
+    *altitude = DecodeAltitudeAsFloat(buf);
+    return MPL_OK;
+}
+
+MPL_OP_STATUS MPL3115A2S_ReadTemperature(float* temperature) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+
+    uint8_t buf[2];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_T_MSB, buf, 2)) return MPL_ERR;
+    *temperature = DecodeTemperatureAsFloat(buf);
+    return MPL_OK;
+}
+
+/* Read Data Functions (integer) ------------------------------------------------------------*/
+
+MPL_OP_STATUS MPL3115A2S_ReadPressureAsInt(int32_t* pressure, uint16_t multiplier) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+    else if(state == MPL_STATE_READY_ALT) return MPL_INVALID_SETTING; // Cannot read pressure in altimeter mode
+
+    uint8_t buf[3];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_P_MSB, buf, 3)) return MPL_ERR;
+    *pressure = DecodePressureAsInt(buf, multiplier);
+    return MPL_OK;
+}
+
+MPL_OP_STATUS MPL3115A2S_ReadAltitudeAsInt(int32_t* altitude, uint16_t multiplier) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+    else if(state == MPL_STATE_READY_PRES) return MPL_INVALID_SETTING; // Cannot read altitude in barometer mode
+
+    uint8_t buf[3];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_P_MSB, buf, 3)) return MPL_ERR;
+    *altitude = DecodeAltitudeAsInt(buf, multiplier);
+    return MPL_OK;
+}
+
+MPL_OP_STATUS MPL3115A2S_ReadTemperatureAsInt(int32_t* temperature, uint16_t multiplier) {
+    if(state == MPL_STATE_NOT_INIT) return MPL_NOT_INIT;
+
+    uint8_t buf[2];
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_OUT_T_MSB, buf, 2)) return MPL_ERR;
+    *temperature = DecodeTemperatureAsInt(buf, multiplier);
+    return MPL_OK;
+}
+
+/* Control Functions -----------------------------------------------------------------------*/
+
+MPL_OP_STATUS MPL3115A2S_Reset() {
+    if (I2C_WriteRegister(MPL3115A2S_ADDR, MPL3115A2S_CTRL_REG1, MPL3115A2S_CTRL_RST)) return MPL_ERR;
+    return MPL_OK;
+}
+
+MPL_OP_STATUS MPL3115A2S_CheckDeviceID() {
+    uint8_t id = 0;
+    if (I2C_ReadRegisters(MPL3115A2S_ADDR, MPL3115A2S_WHO_AM_I, &id, 1)) return MPL_ERR;
+    if (id == MPL3115A2S_WHO_AM_I_ID) return MPL_OK;
+    return MPL_INVALID_ID;
+}
---
+++ MPL3115A2S/MPL3115A2S_Driver.h
@@ -0,0 +1,194 @@
+/**
+ ******************************************************************************
+ * File Name          : MPL3115A2S_Driver.h
+ * Description        : MPL3115A2S Driver
+ *  Supports Polling Mode and Interrupt Mode
+ *  (FIFO Configuration not yet supported)
+ *
+ * Usage                 :
+ *  1. Create an MPL3115A2S_Config, MPL3115A2S_Data_Config, and 
+ *     if interrupt mode is used, the MPL3115A2S_INT_Config structs.
+ *     Define each struct as {0} to use default values, then set the 
+ *     relevant fields as needed.
+ *  2. Call MPL3115A2S_Init() with the desired configurations.
+ * 
+ * Potential Improvements :
+ * - Add offset setting support (OFF_P, OFF_T, OFF_H registers)
+ * - Add FIFO Configuration Support
+ * 
+ * Authors            : Chris (cjchanx)
+ ******************************************************************************
+*/
+#ifndef INCLUDE_MPL3115A2S_DRIVER_H_
+#define INCLUDE_MPL3115A2S_DRIVER_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include "I2CPeripheral_Interface.h" 
+
+/* Macros and Constants ------------------------------------------------------*/
+// Multiplier for fixed point values, e.g. For 101325.5 Pa of pressure
+// the value read would be 101325500. Divide by this value to get the actual value
+#define MPL3115A2S_INT_OUTPUT_MULTIPLIER (1000)
+
+/* Enums and Structs ---------------------------------------------------------*/
+
+// Driver Operation Status, Used as Function Return Values
+typedef enum {
+    MPL3115A2S_OK = 0,
+    MPL_OK = MPL3115A2S_OK,
+    MPL_SUCCESS = MPL3115A2S_OK,
+
+    MPL3115A2S_ERR = 1,
+    MPL_ERR = MPL3115A2S_ERR,
+    MPL_INVALID_SETTING = MPL3115A2S_ERR,
+
+    MPL3115A2S_NOT_INIT = 2,
+    MPL_NOT_INIT = MPL3115A2S_NOT_INIT,
+
+    MPL3115A2S_INVALID_ID = 3,
+    MPL_INVALID_ID = MPL3115A2S_INVALID_ID,
+} MPL_OP_STATUS;
+
+// Control Register 1 Options (CTRL_REG1 = 0x26)
+typedef enum {
+    MPL3115A2S_CTRL_OS128 = 0b111,
+    MPL3115A2S_CTRL_OS64 = 0b110,
+    MPL3115A2S_CTRL_OS32 = 0b101,
+    MPL3115A2S_CTRL_OS16 = 0b100,
+    MPL3115A2S_CTRL_OS8 = 0b011,
+    MPL3115A2S_CTRL_OS4 = 0b010,
+    MPL3115A2S_CTRL_OS2 = 0b001,
+    MPL3115A2S_CTRL_OS1 = 0b000
+} MPL3115A2S_CTRL_OSR;
+
+typedef struct {
+    uint8_t altimeter_mode : 1; // 1 = Altimeter Mode, 0 = Barometer Mode
+    MPL3115A2S_CTRL_OSR os_ratio : 3; // Oversampling Ratio 3-bit value, OS = 2^(os_ratio)
+} MPL3115A2S_Config; 
+
+// Sensor Data Register Options (PT_DATA_CFG = 0x13)
+typedef struct {
+    uint8_t enable_all_flags : 1; // 1 = Enable All Flags (ignore options below)
+
+    uint8_t data_ready_event_mode : 1; // 1 = Data Ready Event Mode, 0 = Polling Mode
+    uint8_t pressure_data_event_flag : 1; // 1 = Enable Pressure Data Event Flag
+    uint8_t temperature_data_event_flag : 1; // 1 = Enable Temperature Data Event Flag
+} MPL3115A2S_Data_Config;
+
+// Interrupt Control Register Options (CTRL_REG3,4 = 0x28,0x29)
+typedef struct {
+    uint8_t int1_active_high : 1; // 1 = Active High, 0 = Active Low
+    uint8_t int1_open_drain : 1;  // 1 = Open Drain, 0 = Internal Pullup
+
+    uint8_t int2_active_high : 1; // 1 = Active High, 0 = Active Low
+    uint8_t int2_open_drain : 1;  // 1 = Open Drain, 0 = Internal Pullup
+
+    uint8_t data_ready_it_enable : 1; // 1 = Data Ready Interrupt Enable
+    uint8_t fifo_it_enable : 1; // 1 = FIFO Interrupt Enable
+
+    uint8_t pressure_window_it_enable : 1; // 1 = Pressure Window Interrupt Enable
+    uint8_t temperature_window_it_enable : 1; // 1 = Temperature Window Interrupt Enable
+    uint8_t pressure_threshold_it_enable : 1; // 1 = Pressure Threshold Interrupt Enable
+    uint8_t temperature_threshold_it_enable : 1; // 1 = Temperature Threshold Interrupt Enable
+    uint8_t pressure_change_it_enable : 1; // 1 = Pressure Change Interrupt Enable
+    uint8_t temperature_change_it_enable : 1; // 1 = Temperature Change Interrupt Enable
+} MPL3115A2S_Int_Config; 
+
+/* Initialization Functions -----------------------------------------------------------------*/
+/**
+ * @brief Initialize the MPL3115A2S Sensor with the given configurations
+ *  Performs WHO_AM_I check, then sets up the sensor with the given configurations
+ * 
+ * @param cfg MPL3115A2S_Config
+ *  Control register 1 configuration, altimeter mode select and oversampling ratio
+ * @param data_cfg MPL3115A2S_Data_Config 
+ *  Data configuration, data ready event mode and data event flag enables
+ * @param int_cfg MPL3115A2S_Int_Config
+ *  Interrupt configuration, interrupt polarity, open drain, and interrupt enables
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_Init(MPL3115A2S_Config* const cfg, MPL3115A2S_Data_Config* const data_cfg, MPL3115A2S_Int_Config* const int_cfg);
+
+/* Read Data Functions (float) ---------------------------------------------------------------*/
+/**
+ * @brief Reads pressure data as a float
+ * Note: The sensor must be in Pressure Mode (not Altitude Mode) to read correct values
+ * 
+ * @param pressure Pointer to value to store data
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_ReadPressure(float* pressure);
+
+/**
+ * @brief Reads altitude data as a float
+ * Note: The sensor must be in Altitude Mode (not Pressure Mode) to read correct values
+ * 
+ * @param altitude Pointer to value to store data
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_ReadAltitude(float* altitude);
+
+/**
+ * @brief Reads temperature data as a float
+ * 
+ * @param temperature Pointer to value to store data
+ * @return MPL_OP_STATUS
+ */
+MPL_OP_STATUS MPL3115A2S_ReadTemperature(float* temperature);
+
+/* Read Data Functions (integer) ------------------------------------------------------------*/
+/**
+ * @brief Reads pressure data as a uint32_t
+ * Note: The sensor must be in Pressure Mode (not Altitude Mode) to read correct values
+ * 
+ * @param multiplier Multiplier for integer value
+ *  e.g. With multiplier 1, 101325.25 Pa of pressure, the value read would be 101325
+ *       With multiplier 1000, the value read would be 101325250
+ * @param pressure Pointer to value to store data
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_ReadPressureInt(uint32_t* pressure, uint16_t multiplier);
+
+/**
+ * @brief Reads altitude data as a int32_t
+ * Note: The sensor must be in Altitude Mode (not Pressure Mode) to read correct values
+ * 
+ * @param multiplier Multiplier for integer value
+ * e.g. With multiplier 1, 12345.5 m of altitude, the value read would be 12345
+ *      With multiplier 1000, the value read would be 12345500
+ * @param altitude Pointer to value to store data
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_ReadAltitudeInt(int32_t* altitude, uint16_t multiplier);
+
+/**
+ * @brief Reads temperature data as a int32_t
+ * 
+ * @param multiplier Multiplier for integer value
+ * e.g. With multiplier 1, 123.25 C of temperature, the value read would be 123
+ *      With multiplier 1000, the value read would be 123250
+ * @param temperature Pointer to value to store data
+ * @return MPL_OP_STATUS
+ */
+MPL_OP_STATUS MPL3115A2S_ReadTemperatureInt(int32_t* temperature, uint16_t multiplier);
+
+/* Control Functions -----------------------------------------------------------------------*/
+/**
+ * @brief Software Reset the MPL3115A2S Sensor
+ *  Note: All register values will be reset to default, must re-init after reset
+ * 
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_Reset();
+
+/**
+ * @brief WHOAMI Check for MPL3115A2S Sensor
+ * 
+ * @param status 
+ * @return MPL_OP_STATUS 
+ */
+MPL_OP_STATUS MPL3115A2S_CheckDeviceID();
+
+
+#endif /* INCLUDE_MPL3115A2S_DRIVER_H_ */
